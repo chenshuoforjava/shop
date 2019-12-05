3139a085e7c9d6c9262c3528b43be959e8cb0a59
chenshuoforjava <282717153@qq.com>
2019-12-06 01:52:18 +0800

    删除

--- src/demo5/Test555.java
+++
@@ -1,8 +0,0 @@
-package demo5;
-
-public class Test555 {
-
-	public static void main(String[] args) {
-		System.out.println("55555555555555");
-	}
-}
--- src/demo5/Test66.java
+++
@@ -1,8 +0,0 @@
-package demo5;
-
-public class Test66 {
-
-	public static void main(String[] args) {
-		System.out.println("6666");
-	}
-}
